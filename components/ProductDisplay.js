46ab33ac357a9c25c29e04464b1dfacd999ffdd4
forio2 <forio2@hotmail.com>
2021-07-08 19:23:55 +0700

    add Shipping

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -1,4 +1,10 @@
 app.component('product-display',{
+    props: {
+        premium: {
+            type: Boolean,
+            required: true
+        }
+    },
     template:
     /*html*/
     `<div class="product-display">
@@ -11,6 +17,7 @@ app.component('product-display',{
             <p v-if="inventory > 10">In Stock</p>
             <p v-else-if="inventory <= 10 && inventory > 0">In Stock</p>
             <p v-else>Out of Stock</p>
+            <p>Shipping: {{shipping}}</p>
             <ul>
                 <li v-for="detail in details">{{ detail }}</li>
             </ul>
@@ -62,6 +69,12 @@ app.component('product-display',{
         },
         inStock(){
             return this.variants[this.selectedVariant].quantity;
+        },
+        shipping(){
+            if(this.premium){
+                return 'Free'
+            }
+            return 30
         }
     }
 })
