58c2bc97eb2eff531e053e477952d3d24f432966
forio2 <forio2@hotmail.com>
2021-07-08 20:12:42 +0700

    chk pt3: create component name product-detail to add detail

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -78,3 +78,22 @@ app.component('product-display',{
         }
     }
 })
+app.component('product-details',{
+    props: {
+        details: {
+            type: Array,
+            required: true
+        }
+    },
+    template:
+        /*html */
+        `<div class="product-details">
+        <div class="product-container">
+            <div class="product-info">
+                <ul>
+                    <li v-for="detail in details">{{ detail }}</li>
+                </ul>
+            </div>
+        </div>
+    </div>`
+})
