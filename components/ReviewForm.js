74a21ae3ab57c9146cad0d3c18f231985ae79b8f
forio2 <forio2@hotmail.com>
2021-07-08 23:35:31 +0700

    console.log review form

--- components/ReviewForm.js
+++ components/ReviewForm.js
@@ -1,29 +1,48 @@
 app.component('review-form',{
     template:
     /*html*/
-    `<form class="review-form">
+    `<form class="review-form" @submit.prevent="onSubmit">
         <h3>Leave a review</h3>
         <label for="name">Name:</label>
-        <input id="name">
+        <input id="name" v-model="name">
 
         <label for="review">Review:</label>
-        <textarea id="review"></textarea>
+        <textarea id="review" v-model="review"></textarea>
 
         <label for="rating">Rating:</label>
-        <select id="rating">
+        <select id="rating" v-model.number="rating">
             <option>5</option>
             <option>4</option>
             <option>3</option>
             <option>2</option>
             <option>1</option>
         </select>
-        <input type="submit" class="button" value="Submit">
+        <input type="submit" class="button" value="Submit" @click="addReview">
     </form>`,
     data(){
         return{
             name: '',
             review: '',
-            rating: null
+            rating: null,
+            reviews:[]
+        }
+    },
+    methods:{
+        onSubmit() {
+            let productReview = {
+                name: this.name,
+                review: this.review,
+                rating: this.rating,
+            }
+            console.log(productReview)
+            this.$emit('review-submitted', productReview)
+
+            this.name = ''
+            this.review = ''
+            this.rating = null
+        },
+        addReview(review){
+            this.reviews.push(review)
         }
     }
 })
