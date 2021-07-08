2917ccd89c25f93899fa9b8ef74ea0deeb348526
forio2 <forio2@hotmail.com>
2021-07-08 19:10:33 +0700

    use components

--- main.js
+++ main.js
@@ -1,45 +1,13 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Shoes',
-            brand: 'SE 331',
-            inventory: 100,
-            details: ['50% cotton', '30% wool', '20% polyester'],
-            variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 }
-            ],
-            cart: 0,
-            selectedVariant: 0,
-            onSale: true,
-
+            cart: 0
         }
     },
     methods: {
-        addToCart() {
-            this.cart += 1
-        },
-        updateImage(variantImage) {
-            this.image = variantImage
-        },
-        updateVariant(index) {
-            this.selectedVariant = index;
-        }
+
     },
     computed: {
-        title(){
-            if(this.onSale){
-                return this.brand + ' ' +this.product + ' is on sale'
-            }
-            else{
-                return this.brand + ' ' + this.product
-            }
-        },
-        image(){
-            return this.variants[this.selectedVariant].image;
-        },
-        inStock(){
-            return this.variants[this.selectedVariant].quantity;
-        }
+
     }
 })
